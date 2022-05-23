fe831e65f6e7f18b4a325c4e9698b6a7934b537f
SURAJ SUNIL JOSHI <surman2825@gmail.com>
2022-05-23 16:16:15 +0530

    Update Linear_and_quadratic_probing.py

--- Linear_and_quadratic_probing.py
+++ Linear_and_quadratic_probing.py
@@ -1,3 +1,7 @@
+'''
+Consider telephone book database of N clients. Make use of a hash table implementation to quickly look up client‘s telephone number. Make use of two collision handling techniques and compare them using number of comparisons required to find a set of telephone numbers'''
+
+
 def linear_probing(db,j,li,n):
     i = 0
     while True:
